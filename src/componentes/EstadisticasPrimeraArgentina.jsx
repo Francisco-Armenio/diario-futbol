486908218e72db24cae2e7dbd604e38d6135ad1a
francisco <franciscoarmenio1@gmail.com>
2025-01-11 02:15:11 -0300

    diseño para la tabla de equipos

--- src/componentes/EstadisticasPrimeraArgentina.jsx
+++ src/componentes/EstadisticasPrimeraArgentina.jsx
@@ -1,41 +1,55 @@
 import React from 'react';
-import primera from './Arrays/ArrayPrimeraDivicion';
+import primera from "./Arrays/ArrayPrimeraDivicion";
 
 const EstadisticasPrimeraArgentina = () => {
     return (
-        <div>
-            <h1>Estadísticas Primera División</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Posición</th>
-                        <th>Nombre</th>
-                        <th>Jugados</th>
-                        <th>Ganados</th>
-                        <th>Empatados</th>
-                        <th>Perdidos</th>
-                        <th>Goles a Favor</th>
-                        <th>Goles en Contra</th>
-                        <th>Diferencia</th>
-                        <th>Puntos</th>
+        <div className='estadisticas-primera-divicion'>
+            <h1 className='titulo-principal'>Estadísticas Primera División</h1>
+            <table className='tabla-de-posiciones'>
+                <thead className='listado'>
+                    <tr className='categoria'>
+                        <th className='puestos'>#</th>
+                        <th className='nombre-de-equipo'>Equipos</th>
+                        <th className='puntos'>PTS</th>
+                        <th className='jugados'>J</th>
+                        <th className='ganados'>G</th>
+                        <th className='empatados'>E</th>
+                        <th className='perdidos'>P</th>
+                        <th className='goles-a-favor'>GF</th>
+                        <th className='goles-en-contra'>GC</th>
+                        <th className='diferencia-de-gol'>+/-</th>
                     </tr>
                 </thead>
-                <tbody>
-                    {primera.map((equipo) => (
-                        <tr key={equipo.id}>
-                            <td>{equipo.pocision}</td>
-                            <td>{equipo.imagen}</td>
-                            <td>{equipo.nombre}</td>
-                            <td>{equipo.jugados}</td>
-                            <td>{equipo.ganados}</td>
-                            <td>{equipo.empatados}</td>
-                            <td>{equipo.perdidos}</td>
-                            <td>{equipo.favor}</td>
-                            <td>{equipo.contra}</td>
-                            <td>{equipo.diferencia}</td>
-                            <td>{equipo.puntos}</td>
-                        </tr>
-                    ))}
+                <tbody className='listado-de-equipos'>
+                    {primera.map((equipo, index) => {
+                        const clasePosicion =
+                            index < 4
+                                ? "equipo-destacado-azul"
+                                : index >= primera.length - 4
+                                ? "equipo-destacado-rojo"
+                                : "";
+
+                        return (
+                            <tr
+                                className={`datos-de-equipos ${clasePosicion}`}
+                                key={equipo.id}
+                            >
+                                <td className='posicion'>{equipo.posicion}</td>
+                                <td className='imagen-de-club'>
+                                    <img src={equipo.imagen} alt="" />
+                                    <h2 className='nombre-de-club'>{equipo.nombre}</h2>
+                                </td>
+                                <td className='partidos-jugados'>{equipo.jugados}</td>
+                                <td className='partidos-ganados'>{equipo.ganados}</td>
+                                <td className='partidos-empatados'>{equipo.empatados}</td>
+                                <td className='partidos-perdidos'>{equipo.perdidos}</td>
+                                <td className='favor'>{equipo.favor}</td>
+                                <td className='contra'>{equipo.contra}</td>
+                                <td className='diferencial'>{equipo.diferencia}</td>
+                                <td className='puntaje'>{equipo.puntos}</td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
         </div>
